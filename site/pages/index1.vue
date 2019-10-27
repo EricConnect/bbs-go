b7b4dd120e5d84761e16815ec698c1356320bf30
EricConnect <33584289+EricConnect@users.noreply.github.com>
2019-10-27 15:14:19 -0400

    Update index1.vue

--- site/pages/index1.vue
+++ site/pages/index1.vue
@@ -21,12 +21,7 @@
             <WidgetUser :user="user" />
             <div style="text-align: center;">
               <!-- 展示广告288x288 -->
-              <ins
-                class="adsbygoogle"
-                style="display:inline-block;width:288px;height:288px"
-                data-ad-client="ca-pub-5683711753850351"
-                data-ad-slot="4922900917"
-              />
+              
               <script>
                 (adsbygoogle = window.adsbygoogle || []).push({});
               </script>
